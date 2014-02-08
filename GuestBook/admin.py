3523382e4f1889d5036ca3d3630d2030421e107a
virusdefender <1670873886@qq.com>
2014-02-08 05:42:37 -0800

    update guest book

--- GuestBook/admin.py
+++ GuestBook/admin.py
@@ -1,1 +1,10 @@
-__author__ = 'LiYang'
+#coding:utf-8
+from django.contrib import admin
+from GuestBook.models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("title", )
+
+
+admin.site.register(Message, MessageAdmin)
